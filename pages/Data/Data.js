9e7c97e34c51092a7beaa409c5725651a57b90fe
Okwor Chukwuemeka <thenaiadev@gmail.com>
2022-10-12 16:55:19 +0100

    finished

---
+++ pages/Data/Data.js
@@ -0,0 +1,26 @@
+const Data = {
+  home: {
+    navLogo: "thenaijadev",
+    name: "Chukwuemeka Okwor",
+    role: "Front-end developer",
+    desc: "Front-end developer who loves to build beautiful and responsive user interfaces using react.",
+  },
+  socials: {
+    linkedIn: "https://www.linkedin.com/in/okwor-chukwuemeka-01568b20b/",
+    github: "https://github.com/thenaijadev",
+    youtube: "https://www.youtube.com/channel/UCJQNsUSo205MS1wHfDxtwqQ",
+    twitter: "https://twitter.com/thenaijadev?t=D6CNl4Meo4FdREho1m8oyg&s=08",
+  },
+  skills: {
+    intro:
+      "I build applications both on web and mobile using react and flutter",
+    skill_1: {
+      title: "Implementation of beautiful designs",
+      desc: " I am able to bring designs to life with pixel perfect accuracy",
+    },
+  },
+
+  technologies: ["Javascript", "Html", "Css"],
+};
+
+export default Data;
